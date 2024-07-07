b26edb029ec66bbe7d9acb7fece67cfd4f9ba5d0
ToppTobi <tobias.topp@gmx.ch>
2024-07-07 18:50:41 +0200

    Added Coins and a Jump Buffer

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,52 +1,64 @@
 extends CharacterBody2D
 
-
-const SPEED = 150.0
-const JUMP_VELOCITY = -300.0
-
+@export var SPEED = 150.0
+@export var MAX_JUMP_VELOCITY = -300.0
+@export var MIN_JUMP_VELOCITY = -250.0
+@export var jump_Buffer_Time := 0.01
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var max_jump_timer = $MaxJumpTimer
+
+var jump_Buffer := false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		if jump_Buffer:
+			jump()
+			jump_Buffer = false
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
 		
-		
+		if is_on_floor():
+			jump()
+		else:
+			jump_Buffer = true
+			get_tree().create_timer(jump_Buffer_Time).timeout.connect(on_jump_buffer_timeout)
+
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
-	
-	#Flip Character
+
+	# Flip Character
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-		
-	#Play Animations
+
+	# Play Animations
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
-	else: 
+	else:
 		animated_sprite.play("jump")
-		
-	#Apply movement
+
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
 
+func jump() -> void:
+	velocity.y = MAX_JUMP_VELOCITY
 
+func on_jump_buffer_timeout() -> void:
+	jump_Buffer = false
 
 
